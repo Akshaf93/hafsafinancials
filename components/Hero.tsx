664ce77f1190a65441e5671ff030f8e3c627be69
Akshaf93 <akshaf93@gmail.com>
2026-01-06 16:31:37 +0500

    sd

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -140,7 +140,7 @@ export default function Hero() {
           <div 
             className="absolute inset-0 w-full h-full bg-contain bg-no-repeat bg-center opacity-80"
             style={{ 
-              backgroundImage: "url('https://upload.wikimedia.org/wikipedia/commons/8/80/World_map_-_low_resolution.svg')",
+              backgroundImage: "url('public/World_map_-_low_resolution.svg')",
               filter: "invert(1) sepia(1) saturate(0.2) brightness(0.7)" 
             }}
           />
